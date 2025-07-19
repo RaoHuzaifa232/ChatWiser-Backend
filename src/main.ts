aeef56aba2bcc72f84e2c92fa84286d67799f5c3
Rao_Huzaifa <mhuzaifarao232@gmail.com>
2025-07-20 00:41:06 +0500

    `Added app.enableCors() and app.listen() configuration`

--- src/main.ts
+++ src/main.ts
@@ -11,6 +11,7 @@ async function bootstrap() {
     .build();
   const documentFactory = () => SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, documentFactory);
+  app.enableCors();
   await app.listen(process.env.PORT ?? 3000);
 }
 void bootstrap();
